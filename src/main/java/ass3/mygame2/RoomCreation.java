26279d9fde4bce731e2ebe5f835cbe4ecb865968
BhavinDhorajiya <81555139+BhavinDhorajiya@users.noreply.github.com>
2021-06-05 01:09:46 +0800

    Sprint 4 - Game tracing
    Add feature to allow the user to navigate from the kitchen by adding an extra room and setting the exits

--- src/main/java/ass3/mygame2/RoomCreation.java
+++ src/main/java/ass3/mygame2/RoomCreation.java
@@ -17,20 +17,24 @@ public class RoomCreation {
     
     private void createRooms() {
 
-        Room castle, kitchen, frontGate;
+        Room castle, kitchen, frontGate,livingRoom;
 
         castle = new Room("castle", "You are at the castle", false);
         kitchen = new Room("kitchen", "The kitchen door has a shape of a heart", false);
         frontGate = new Room("frontGate", "There is a giant ogre", true);
+        livingRoom = new Room("LivingRoom", "The Warrior rests", false);
         
 
         castle.setExit("east", kitchen);
         castle.setExit("south", frontGate);
         frontGate.setExit("north", castle);
+        kitchen.setExit("west",livingRoom);
+        livingRoom.setExit("north",castle);
 
         castle.addItemInRoom(itemCreation.getItem("excaliburSword"));
         castle.addItemInRoom(itemCreation.getItem("key"));
         kitchen.addItemInRoom(itemCreation.getItem("frontGateKey"));
+        livingRoom.addItemInRoom(itemCreation.getItem("Magical Herb"));
 
         allRoomInGame.add(castle);
         allRoomInGame.add(frontGate);
